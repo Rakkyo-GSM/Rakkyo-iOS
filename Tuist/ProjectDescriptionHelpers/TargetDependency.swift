b2f37a30ee976e6725874b8c58a9196b69e70d4e
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-08-09 22:51:02 +0900

    :page_facing_up: FScalendar 추가

--- Tuist/ProjectDescriptionHelpers/TargetDependency.swift
+++ Tuist/ProjectDescriptionHelpers/TargetDependency.swift
@@ -7,6 +7,7 @@ extension TargetDependency{
 public extension TargetDependency.SPM{
     static let SnapKit = TargetDependency.package(product: "SnapKit")
     static let Then = TargetDependency.package(product: "Then")
+    static let FSCalendar = TargetDependency.package(product: "FSCalendar")
 }
 
 public extension Package {
@@ -17,6 +18,10 @@ public extension Package {
     static let Then = Package.remote(
         url: "https://github.com/devxoul/Then.git",
         requirement: .upToNextMajor(from: "3.0.0")
+    )
+    static let FSCalendar = Package.remote(
+        url: "https://github.com/Alamofire/Alamofire",
+        requirement: .upToNextMajor(from: "5.6.1")
     )
 }
 
