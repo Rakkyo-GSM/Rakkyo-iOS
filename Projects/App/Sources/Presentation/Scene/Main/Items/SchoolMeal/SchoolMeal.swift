94e861f877de1e18686d5acfdb4a3270605706fd
Heesang <ghs050611@naver.com>
2022-08-22 15:05:20 +0900

    :memo: :: menuListView create

--- Projects/App/Sources/Presentation/Scene/Main/Items/SchoolMeal/SchoolMeal.swift
+++ Projects/App/Sources/Presentation/Scene/Main/Items/SchoolMeal/SchoolMeal.swift
@@ -3,8 +3,13 @@ import UIKit
 
 final class SchoolMealVC: baseVC{
     
+    private let menuLabel = UILabel().then(){
+        $0.text = "급식"
+    }
     
-    
-    
+    private let dateButton = UIButton().then(){
+        $0.setTitle("20xx년 xx월 xx일", for: .normal)
+        $0.backgroundColor = RakkyoIOSAsset.Colors.rakkyoMainColor.color
+    }
     
 }
---
+++ Projects/App/Sources/Presentation/Scene/Main/Items/SchoolMeal/View/MenuListView.swift
@@ -0,0 +1,10 @@
+import UIKit
+
+final class MenuListView: UIView{
+    
+    
+    
+    
+    
+    
+}
