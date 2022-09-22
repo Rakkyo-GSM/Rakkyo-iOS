9ad05a71c008a9744cfcb4d57f18645262cd6c08
Limjunhwa <76590302+pasin7793@users.noreply.github.com>
2022-09-22 19:58:13 +0900

    :fire: 안쓰는 파일

--- Projects/App/Sources/Util/Enum/Image.swift
+++
@@ -1,8 +0,0 @@
-import UIKit
-
-enum Image{
-    static let logoImage = UIImage(named: "Rakkyo-logo")
-    static let calendarImage = UIImage(systemName: "calendar")
-    static let schoolMealImage = UIImage(systemName: "fork.knife")
-    static let scheulerImage = UIImage(systemName: "list.bullet.rectangle.portrait")
-}
